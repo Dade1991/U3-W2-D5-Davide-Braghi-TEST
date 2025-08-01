77c0aabbcb2a3cc30192cac868120b57f6096160
Dade1991 <dade91@msn.com>
2025-08-01 10:52:36 +0200

    Added some tools

---
+++ weather-web-app/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import CustomNavbar from "./components/CutomNavbar"
+
+function App() {
+  return (
+    <>
+      <CustomNavbar />
+    </>
+  )
+}
+
+export default App
